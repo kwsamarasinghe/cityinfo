c4b580da06805181f1c63c32f1d69bb5a1b8e241
Kasun Samarasinghe <kwsamarasinghe@gmail.com>
2018-03-04 14:24:24 +0100

    configuration folder addded

--- src/engine/apihandler.py
+++
@@ -1,8 +0,0 @@
-#This class is the main component, which calls the relevant services and composes the info
-class APIHandler:
-
-    __init__():
-        print "initializes the info engine"
-
-    #Execute the clients
-    def extractData():
